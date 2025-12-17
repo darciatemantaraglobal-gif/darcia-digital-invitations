53bfe46bf060621669d571baaa4460af4580ff94
darciatemantaraglobal-gif <darciatemantaraglobal@gmail.com>
2025-12-17 17:50:32 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,29 +4,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+
+// Import Halaman
 import Index from "./pages/Index";
 import Catalog from "./pages/Catalog";
 import Order from "./pages/Order";
 import NotFound from "./pages/NotFound";
-// 1. IMPORT INI JANGAN LUPA
 import PreviewFrame from "./pages/PreviewFrame"; 
-// ... import yang lain
 import Generator from "./pages/Generator";
-
-const App = () => (
-  // ... rest of code
-  <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="/catalog" element={<Catalog />} />
-    <Route path="/generate" element={<Generator />} /> {/* <--- TAMBAHIN INI */}
-    <Route 
-      path="/siskaridho" 
-      element={<PreviewFrame url="https://darciasiskaridho.vercel.app" />} 
-    />
-    <Route path="*" element={<NotFound />} />
-  </Routes>
-  // ... rest of code
-);
 
 const queryClient = new QueryClient();
 
@@ -38,17 +23,21 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
+            {/* 1. Halaman Utama */}
             <Route path="/" element={<Index />} />
             <Route path="/catalog" element={<Catalog />} />
             <Route path="/order" element={<Order />} />
             
-            {/* 2. JALUR KHUSUS SISKA (Taruh SEBELUM NotFound) */}
+            {/* 2. Tool Generator Link (darcia.id/generate) */}
+            <Route path="/generate" element={<Generator />} /> 
+            
+            {/* 3. Preview Undangan Siska (darcia.id/siskaridho) */}
             <Route 
               path="/siskaridho" 
               element={<PreviewFrame url="https://darciasiskaridho.vercel.app" />} 
             />
 
-            {/* Catch-all route harus paling bawah */}
+            {/* 4. Catch-all (Harus paling bawah) */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
